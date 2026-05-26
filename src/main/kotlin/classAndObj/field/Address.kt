800bff932395b23dcaf122fe3a59c2bcd493ce4b
lihao <2390183798@qq.com>
2026-05-26 16:19:19 +0800

    属性 相关

---
+++ src/main/kotlin/classAndObj/field/Address.kt
@@ -0,0 +1,19 @@
+package org.dodo.classAndObj.field
+
+class Address {
+    var name: String = "Holmes, Sherlock"
+    var street: String = "Baker"
+    var city: String = "London"
+    var state: String? = null
+    var zip: String = "123456"
+
+    fun copyAddress(address: Address): Address {
+        val result = Address() // Kotlin 中没有“new”关键字
+        result.name = address.name // 将调用访问器
+        result.street = address.street
+        result.city = address.city
+        result.state = address.state
+        result.zip = address.zip
+        return result
+    }
+}
---
+++ src/main/kotlin/classAndObj/field/READ_ME.md
@@ -0,0 +1,9 @@
+### 简单说明
+
+#### Address
+- 对应的属性
+- 方法，copy对象
+
+
+#### Rectangle
+- 定义 area 的 get() 方法， 直接用 对象.area 调用 get()方法
