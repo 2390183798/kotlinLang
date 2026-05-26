f320643cbd02aedcd87710fe7f82fd4e498ba059
lihao <2390183798@qq.com>
2026-05-26 16:35:48 +0800

    属性 相关

--- src/main/kotlin/classAndObj/field/READ_ME.md
+++ src/main/kotlin/classAndObj/field/READ_ME.md
@@ -7,3 +7,9 @@
 
 #### Rectangle
 - 定义 area 的 get() 方法， 直接用 对象.area 调用 get()方法
+- 在 属性下面添加 private set
+  - 外部不能 set
+  - 内部可以 set
+- 在 set 中判断后 （幕后字段）
+  - 可以用 field = value 处理当前 属性
+- 
--- src/main/kotlin/classAndObj/field/Rectangle.kt
+++ src/main/kotlin/classAndObj/field/Rectangle.kt
@@ -3,9 +3,48 @@ package org.dodo.classAndObj.field
 class Rectangle(val width: Int, val height: Int) {
     val area: Int // property type is optional since it can be inferred from the getter's return type
         get() = this.width * this.height
+
+    var rep : String = "rep"
+        private set
+
+    var stringRepresentation: String
+        get() = this.toString()
+        set(value) {
+            setDataFromString(value) // 解析字符串并赋值给其他属性
+        }
+
+    var counter = 0 // 这个初始器直接为幕后字段赋值
+        set(value) {
+            if (value >= 0)
+                field = value
+            // counter = value // ERROR StackOverflow: Using actual name 'counter' would make setter recursive
+        }
+
+    val isEmpty: Boolean
+        get() = this.counter == 0
+
+    fun setDataFromString(value: String){
+        this.rep = value
+    }
+
+    override fun toString(): String {
+        return "Rectangle(width=$width, height=$height, area=$area, rep='$rep'), counter='${counter}'"
+    }
+
+
 }
 //sampleEnd
 fun main() {
     val rectangle = Rectangle(3, 4)
     println("Width=${rectangle.width}, height=${rectangle.height}, area=${rectangle.area}")
+    println("stringRepresentation=${rectangle.stringRepresentation}")
+
+    rectangle.stringRepresentation = "newRep"
+    rectangle.counter = 5
+//    rectangle.rep = "aa"  // private set 不能外部调用
+
+    println("Width=${rectangle.width}, height=${rectangle.height}, area=${rectangle.area}")
+    println("stringRepresentation=${rectangle.stringRepresentation}")
+
+
 }
