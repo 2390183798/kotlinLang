dba184746579f3b52c5fe6bdfc8e698cdb406b08
lihao <2390183798@qq.com>
2026-05-31 10:35:42 +0800

    整理 invoke写法 到 常规函数

--- src/main/kotlin/func/high/h01/High.kt
+++ src/main/kotlin/func/high/h01/High.kt
@@ -1,4 +1,4 @@
-package org.dodo.func.high
+package org.dodo.func.high.h01
 
 class High {
 }
@@ -20,22 +20,26 @@ fun main() {
     val items = listOf(1, 2, 3, 4, 5)
 
     // Lambdas 表达式是花括号括起来的代码块。
-    items.fold(0, {
+    val result = items.fold(0) {
         // 如果一个 lambda 表达式有参数，前面是参数，后跟“->”
-            acc: Int, i: Int ->
-        print("acc = $acc, i = $i, ")
-        val result = acc + i
+        acc: Int, next: Int ->
+        print("acc = $acc, next = $next, ")
+        val result = acc + next
         println("result = $result")
         // lambda 表达式中的最后一个表达式是返回值：
         result
-    })
+    }
+    println()
+    println("01 result = $result")
 
     // lambda 表达式的参数类型是可选的，如果能够推断出来的话：
-    val joinedToString = items.fold("Elements:", { acc, i -> "$acc $i" })
+    val joinedToString = items.fold("Elements:") { acc, i -> "$acc $i" }
 
     // 函数引用也可以用于高阶函数调用：
     val product = items.fold(1, Int::times)
     //sampleEnd
-    println("joinedToString = $joinedToString")
-    println("product = $product")
+    println()
+    println("02 joinedToString = $joinedToString")
+    println()
+    println("03 product = $product")
 }
---
+++ src/main/kotlin/func/simple/invoke/Main.kt
@@ -0,0 +1,17 @@
+package org.dodo.func.simple.invoke
+
+fun main() {
+    //sampleStart
+    val stringPlus: (String, String) -> String = String::plus
+    val intPlus: Int.(Int) -> Int = Int::plus
+
+    println(stringPlus.invoke("<-", "->"))
+    println(stringPlus("<-", "->"))
+    println(stringPlus.invoke("Hello, ", "world!"))
+    println(stringPlus("Hello, ", "world!"))
+
+    println(intPlus.invoke(1, 1))
+    println(intPlus(1, 2))
+    println(2.intPlus(3)) // 类扩展调用
+    //sampleEnd
+}
